178dd31d472a99eefc7c3088bc6e3827beacae6b
NyxOkkotsu <farisamrullah4321@Gmail.com>
2026-06-29 21:16:04 +0700

    Add functions to manage official packages

--- internal/build/makepkg.go
+++ internal/build/makepkg.go
@@ -22,7 +22,35 @@ func CleanupDependencies() error {
 
 func RemovePackage(pkgName string) error {
 	cmd := exec.Command("sudo", "pacman", "-Rns", pkgName)
+	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
+}
+
+func IsOfficialPackage(pkgName string) bool {
+	cmd := exec.Command("pacman", "-Sp", pkgName)
+	return cmd.Run() == nil
+}
+
+func InstallOfficialPackage(pkgName string) error {
+	cmd := exec.Command("sudo", "pacman", "-S", pkgName)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func PrintOfficialSearch(query string) {
+	cmd := exec.Command("pacman", "-Ss", query)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	_ = cmd.Run()
+}
+
+func PrintOfficialInfo(pkgName string) bool {
+	cmd := exec.Command("pacman", "-Si", pkgName)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run() == nil
 }
